764691d63dd9b3e9020437c79d10074f1b5c139c
mvcbox <mvcbox.org@gmail.com>
2022-07-17 14:01:38 +0300

    Update errorToStderr()

--- src/interface/http/rpc/foundation/error-to-stderr.ts
+++ src/interface/http/rpc/foundation/error-to-stderr.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
-export function errorToStderr(err: any): void {
-  console.error(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]:`, err);
+export function errorToStderr(...args: any[]): void {
+  console.error(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]:`, ...args);
 }
