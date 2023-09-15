ea85882c98b1d193a3e9f3f70f0b20c081a2ad25
mvcbox <mvcbox.org@gmail.com>
2023-09-15 19:15:55 +0300

    Add env var DATABASE_SSL_ENABLE

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -5,10 +5,12 @@ const { config } = require('../../dist/config');
 module.exports = {
   production: {
     ...config.database.replication.write,
-    dialect: config.database.dialect
+    dialect: config.database.dialect,
+    ssl: config.database.ssl
   },
   development: {
     ...config.database.replication.write,
-    dialect: config.database.dialect
+    dialect: config.database.dialect,
+    ssl: config.database.ssl
   }
 };
